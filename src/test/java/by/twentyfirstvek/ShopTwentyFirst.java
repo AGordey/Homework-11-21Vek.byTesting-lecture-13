56db119093ae039592bf7926d21437f353fefcd9
AGordey <gordey.andrej@gmail.com>
2022-05-21 21:30:22 +0300

    21vek

--- src/test/java/by/twentyfirstvek/ShopTwentyFirst.java
+++ src/test/java/by/twentyfirstvek/ShopTwentyFirst.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 
-public class TestingFormTests extends TestBase {
+public class ShopTwentyFirst extends TestBase {
 
     @Order(2)
     @Test
