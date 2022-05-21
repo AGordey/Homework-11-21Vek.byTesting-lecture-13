f5c33eb869e68f96ef9a045bb1fa2b0030bafe38
AGordey <gordey.andrej@gmail.com>
2022-05-21 14:07:49 +0300

    21vek

--- src/test/java/by/twentyfirstvek/TestingFormTests.java
+++ src/test/java/by/twentyfirstvek/TestingFormTests.java
@@ -1,11 +1,10 @@
-package com.demoqa;
+package by.twentyfirstvek;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
